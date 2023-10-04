ef831952cebfcd12d72a28cd2a67c4c92ef99525
fredtaran <ftaran04@gmail.com>
2023-10-04 13:57:44 +0800

    Employee Management Feature

---
+++ src/personnel/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+
+urlpatterns = [
+    path('', list_of_personnel, name="list-of-personnel")
+]
--- src/personnel/views.py
+++ src/personnel/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def list_of_personnel(request):
+    
+    context = {}
+
+    return render(request, 'list-of-employee.html', context)
