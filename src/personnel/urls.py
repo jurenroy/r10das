08e0e860c6c1704dc7bff72d481031b32d2e5d0d
fredtaran <ftaran04@gmail.com>
2023-10-04 17:22:03 +0800

    Create Form for adding employee

--- src/personnel/urls.py
+++ src/personnel/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path('', list_of_personnel, name="list-of-personnel")
+    path('', list_of_personnel, name="list-of-personnel"),
+    path('new-employee/', add_employee, name="add-employee")
 ]
--- src/personnel/views.py
+++ src/personnel/views.py
@@ -6,3 +6,8 @@ def list_of_personnel(request):
     context = {}
 
     return render(request, 'list-of-employee.html', context)
+
+def add_employee(request):
+
+    context = {}
+    return render(request, 'new-employee.html', context)
